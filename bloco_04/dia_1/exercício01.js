248bc0d8a9dd8887c19d3e81c0b492df4041e262
luis carlos <luizcruz0809@gmail.com>
2021-05-19 18:53:48 -0300

    Adiciona Bloco04 e atualiza repositorios anteriores

---
+++ bloco_04/dia_1/exercício01.js
@@ -0,0 +1,10 @@
+const name = 'luis';
+const birthCity = 'Brasilia';
+let birthYear = 1994;
+birthYear = 2030;
+birthCity = 'sp';
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+console.log(birthYear);
---
+++ bloco_04/dia_1/exercício05.js
@@ -0,0 +1,27 @@
+let status = "Reprovada";
+
+switch (status) {
+  case "Aprovada":
+    console.log("Aprovada");
+    break;
+
+    case "lista":
+    console.log("lista");
+    break;
+
+    case "Reprovada":
+    console.log("Reprovada");
+    break;
+
+    default:
+}
+
+
+
+
+
+
+
+
+/*Crie uma variável para armazenar o estado da pessoa candidata no processo seletivo, que pode ser: 'aprovada' , 'lista' ou 'reprovada' ;
+Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior se o estado da pessoa candidata for 'aprovada' , 'lista' ou 'reprovada' . Como default , imprima a mensagem de 'não se aplica' .*/
