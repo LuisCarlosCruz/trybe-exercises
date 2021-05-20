152b04d7f22ba3702dbe50bd44f905df90eff55f
luis carlos <luizcruz0809@gmail.com>
2021-05-20 18:30:52 -0300

    Adiciona pasta exercícios dia 02 JS

---
+++ bloco_4/dia_02/script00.js
@@ -0,0 +1,15 @@
+let list = ['mae', 'luis', 'jose'];
+
+list.push('toia', 'teo'); //adiciona item a lista
+  // console.log(list);
+
+list.pop('teo');    // remove item
+  // console.log(list);
+
+list.shift();  //remove o primeiro item da lista
+  // console.log(list);
+
+let searchList = list.indexOf('toia'); //mostra o indice do item
+  console.log(searchList);
+  console.log(list);
+
---
+++ bloco_4/dia_02/script01.js
@@ -0,0 +1,5 @@
+let menu = ['home', 'serviços', 'portifólio', 'links'];
+let menuServices = menu[1];
+console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu :
