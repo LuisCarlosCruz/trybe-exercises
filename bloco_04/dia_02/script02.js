1136a719b047f9934b2da283777657e331fc04ea
luis carlos <luizcruz0809@gmail.com>
2021-07-20 20:36:29 -0300

    rename

--- bloco_04/dia_02/script02.js
+++
@@ -1,7 +0,0 @@
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
-
-console.log(indexOfPortfolio);
-
-// Procure o índice do valor "Portfólio" do array menu :
