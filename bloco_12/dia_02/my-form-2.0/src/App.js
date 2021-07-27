5c4cd998d3343215b31789510cbb64786e703153
luis carlos <luizcruz0809@gmail.com>
2021-07-27 11:18:03 -0300

    add part_01

---
+++ bloco_12/dia_02/my-form-2.0/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Fieldset from './componentes/Fieldset';
+
+function App() {
+  return (
+    <form className='form'>
+      <Fieldset />
+    </form>
+  );
+}
+
+export default App;
