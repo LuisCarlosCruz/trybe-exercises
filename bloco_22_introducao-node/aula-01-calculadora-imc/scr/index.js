44a08317113d68c6b6d0490b43a65b9f964f5d3f
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-01-09 14:57:32 -0300

    calculadora imc

--- bloco_22_introducao-node/aula-01-calculadora-imc/scr/index.js
+++ bloco_22_introducao-node/aula-01-calculadora-imc/scr/index.js
@@ -3,5 +3,5 @@ const {nome, altura, peso} = require('./questions.js');
 // calcula imc  imc = kg / m²
 const imc = peso / (altura * 2);
 
-// imprime
+// imprime IMC
 console.log(`${nome} seu IMC é ${imc.toFixed(2)}`);
