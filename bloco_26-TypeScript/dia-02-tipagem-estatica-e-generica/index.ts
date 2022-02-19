c7efbca3bbee177b5530ad256ab47628158dd22b
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-02-18 22:48:27 -0300

    type union

--- bloco_26-TypeScript/dia-02-tipagem-estatica-e-generica/index.ts
+++ bloco_26-TypeScript/dia-02-tipagem-estatica-e-generica/index.ts
@@ -82,3 +82,23 @@ type address = {
   city: string;
   state: string;
 }
+
+// -------------------------------------------------------------------------
+
+
+// TYPE UNIONS
+
+// Crie um type union que represente os estados físicos da matéria: líquido, sólido ou gasoso.
+type states = "liquid" | "solid" | "gaseous";
+
+
+// Crie um type union que represente o documento identificador de uma pessoa que pode receber valores numéricos ou texto. Ex: “123.567.890-12” ou 123456789012.
+type doc = string | number;
+
+
+// Crie um type union que represente sistemas operacionais: linux, mac os ou windows.
+type SO = "linux" | "mac" | "window";
+
+
+// Crie um type union que represente as vogais do alfabeto.
+type vogais = "a" | "e" | "i" | "o" | "u";
