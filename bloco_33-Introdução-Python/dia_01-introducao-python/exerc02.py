55a423ab775d2b165d773584185e833edb515617
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-08-03 20:19:03 -0300

    add exer 02

--- bloco_33-Introdução-Python/dia_01-introducao-python/exerc02.py
+++ bloco_33-Introdução-Python/dia_01-introducao-python/exerc02.py
@@ -1,15 +1,14 @@
 # Calcule a média aritmética dos valores contidos em uma lista.
 
-from cv2 import minEnclosingCircle
-
-
 def media_aritmetica(*elements):
     list = []
+
     for num in elements:
         list.append(num)
 
     length_lista = len(list)
-    return length_lista
+
+    return sum(list) / length_lista
 
 
-print(media_aritmetica(1, 2, 3))
+print(media_aritmetica(1, 2, 3, 4))
